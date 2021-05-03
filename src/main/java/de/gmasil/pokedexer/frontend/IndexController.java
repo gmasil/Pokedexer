beb1d4e0ce521f89b2830373b0baf57d2b9adc57
gmasil <54176035+gmasil@users.noreply.github.com>
2021-05-03 16:58:41 +0200

    update license template

--- src/main/java/de/gmasil/pokedexer/frontend/IndexController.java
+++ src/main/java/de/gmasil/pokedexer/frontend/IndexController.java
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Pokédexer.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Pokédexer. If not, see <https://www.gnu.org/licenses/>.
  */
 package de.gmasil.pokedexer.frontend;
 
