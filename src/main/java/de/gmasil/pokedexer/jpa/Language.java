992203c0fc866c303bfc04d3e71140035156f726
gmasil <54176035+gmasil@users.noreply.github.com>
2021-07-02 02:01:41 +0200

    fix recursive marshalling of Language entity in REST API

--- src/main/java/de/gmasil/pokedexer/jpa/Language.java
+++ src/main/java/de/gmasil/pokedexer/jpa/Language.java
@@ -39,6 +39,7 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AccessLevel;
@@ -68,6 +69,7 @@ public class Language implements Serializable {
     @Column(unique = true, nullable = false)
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "language")
     private Set<Card> cards;
 
